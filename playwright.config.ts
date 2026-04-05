5447e94accf2ba6f2d60cf4a2b193b4dba733bea
Vien Bui <buivien314@gmail.com>
2026-04-05 22:18:27 +0700

    implement BS007

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://marsair.recruiting.thoughtworks.net/VienBui',
+    baseURL: 'https://marsair.recruiting.thoughtworks.net',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
--- tests/findFlight.spec.ts
+++ tests/findFlight.spec.ts
@@ -1,27 +1,30 @@
 import { test, expect, Page } from '@playwright/test';
-import { HomePageLocators } from '../src/locators/homepage.locator';
 import { HomePage } from '../src/pages/homepage.page';
+import { SearchPage } from '../src/pages/search.page';
+import { SearchResultPage } from '../src/pages/searchResult.page';
+import { PERIOD_MORE_THAN_2_YEAR_SELECT_RANGE } from '../src/data/selectRange';
 
-test.describe('Home Page', () => {
+test.describe('Search Flight', () => {
 
     // BS-001: Verify Home page displays with all elements
     test('Verify home page is loaded with all elements visible', async ({ page }) => {
         const homePage = new HomePage(page);
         await homePage.navigateToHomePage();
-        await homePage.verifyHomePageIsLoaded();
-        await homePage.verifyHeaderLogoIsVisible();
-        await homePage.verifySelectDepartingOptionIsVisible();
-        await homePage.verifySelectReturningOptionIsVisible();
-        await homePage.verifySearchButtonIsVisible();
 
-        await homePage.verifyPromoCodeInputIsVisible();
+        await homePage.verifyHomePageIsLoaded();
+
+        await homePage.verifyHeaderLogoIsVisible();
         await homePage.verifyReportIssueLinkIsVisible();
         await homePage.verifyProblemDefinitionLinkIsVisible();
         await homePage.verifyPrivacyPolicyLinkIsVisible();
-        await homePage.verifyBookTicketNowMessageIsVisible();
+
         await homePage.verifyWelcomeMessageIsVisible();
-        await homePage.verifyAllDepartingOptionsAreVisible();
-        await homePage.verifyAllReturningOptionsAreVisible();   
+        await homePage.verifyBookTicketNowMessageIsVisible();
+        await homePage.verifySelectDepartingOptionIsVisible();
+        await homePage.verifySelectReturningOptionIsVisible();
+        await homePage.verifyPromoCodeInputIsVisible();
+        await homePage.verifySearchButtonIsVisible();
+         
     });
 
     // BS-002: Verify Departing dropdown values
@@ -38,4 +41,18 @@ test.describe('Home Page', () => {
         await homePage.verifyAllReturningOptionsAreVisible();
     });
 
+
+    // BS-007: Verify system validates for available seat once user searches with period is more than 2 years
+    test('Verify system validates for available seat once user searches with period is more than 2 years', async ({ page }) => {
+        const searchPage = new SearchPage(page);
+        const homePage = new HomePage(page);
+        const searchResultPage = new SearchResultPage(page);
+        await homePage.navigateToHomePage();
+        await searchPage.searchFlight(PERIOD_MORE_THAN_2_YEAR_SELECT_RANGE[0].departing, PERIOD_MORE_THAN_2_YEAR_SELECT_RANGE[0].returning);
+        await searchResultPage.verifySearchResultTitleIsVisible();
+        await searchResultPage.verifySeatsAvailableMessageIsVisible();
+       
+        await searchResultPage.verifyCallNowMessageIsVisible();
+        await searchResultPage.verifyBackButtonIsVisible();
+    });
 });
